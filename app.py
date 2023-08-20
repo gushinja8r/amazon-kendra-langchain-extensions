4a0817c52057fa9293d49115dc08e559b2d1c91e
Gary A. Stafford <4302597+gushinja8r@users.noreply.github.com>
2023-08-19 21:17:56 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,260 @@
+# Conversational Retrieval QA Chatbot, built using Langflow and Streamlit
+# Author: Gary A. Stafford
+# Date: 2023-08-19
+# Requirements: pip install -r requirements.txt -U
+# Usage: streamlit run app.py <flanxl|flanxxl|llama2chat|bedrockclaude|bedrocktitan|bedrockai21labs|openai> --server.runOnSave true
+
+import os
+import sys
+
+import streamlit as st
+
+from model_providers import (
+    kendra_chat_bedrock_anthropic_claude as bedrockclaude,
+    kendra_chat_bedrock_amazon_titan as bedrocktitan,
+    kendra_chat_bedrock_ai21_labs as bedrockai21labs,
+    kendra_chat_open_ai as openai,
+    kendra_chat_flan_xxl as flanxxl,
+    kendra_chat_flan_xl as flanxl,
+    kendra_chat_llama2_chat as llama2chat,
+)
+
+# ****** CONFIGURABLE PARAMETERS ******
+USER_ICON = os.environ.get("USER_ICON", "images/user-icon.png")
+AI_ICON = os.environ.get("AI_ICON", "images/ai-icon.png")
+HEADER_TITLE = os.environ.get("HEADER_TITLE", "A Chatbot powered by Amazon Kendra")
+HEADER_LOGO = os.environ.get("HEADER_LOGO", "images/ai-icon.png")
+PAGE_TITLE = os.environ.get("PAGE_TITLE", "AI Chatbot")
+PAGE_FAVICON = os.environ.get("PAGE_FAVICON", "images/ai-icon.png")
+SHOW_DOC_SOURCES = os.environ.get("SHOW_DOC_SOURCES", True)
+TEXT_INPUT_PROMPT = os.environ.get(
+    "TEXT_INPUT_PROMPT", "Ask me any question about Amazon SageMaker."
+)
+TEXT_INPUT_PLACEHOLDER = os.environ.get(
+    "TEXT_INPUT_PLACEHOLDER", "What is Amazon SageMaker?"
+)
+TEXT_INPUT_HELP = os.environ.get(
+    "TEXT_INPUT_HELP",
+    "For more help, see our official documentation: https://docs.aws.amazon.com/sagemaker/index.html",
+)
+SHOW_SAMPLE_QUESTIONS = os.environ.get("SHOW_SAMPLE_QUESTIONS", True)
+MAX_HISTORY_LENGTH = os.environ.get("MAX_HISTORY_LENGTH", 5)
+PROVIDER_MAP = {
+    "openai": os.environ.get("OPENAI_MODEL_NAME", "OpenAI GPT-3.5 Turbo"),
+    "flanxl": os.environ.get("FLANXL_MODEL_NAME", "Flan-T5-XL"),
+    "flanxxl": os.environ.get("FLANXXL_MODEL_NAME", "Flan-T5-XXL"),
+    "llama2chat": os.environ.get("LLAMA_MODEL_NAME", "Llama-2 13B Chat"),
+    "bedrockclaude": os.environ.get(
+        "BEDROCK_CLAUDE_MODEL_NAME", "Bedrock Anthropic Claude Instant v1.1"
+    ),
+    "bedrocktitan": os.environ.get(
+        "BEDROCK_TITAN_MODEL_NAME", "Titan Text Large v1.01"
+    ),
+    "bedrockai21labs": os.environ.get(
+        "BEDROCK_AI21_LABS_MODEL_NAME", "AI21 Labs Jurassic-2 Ultra v1"
+    ),
+}
+
+
+def main():
+    st.set_page_config(
+        page_title=PAGE_TITLE,
+        page_icon=PAGE_FAVICON,
+    )
+
+    if "llm_chain" not in st.session_state:
+        if len(sys.argv) > 1:
+            if sys.argv[1] == "flanxl":
+                st.session_state["llm_app"] = flanxl
+                st.session_state["llm_chain"] = flanxl.build_chain()
+            elif sys.argv[1] == "flanxxl":
+                st.session_state["llm_app"] = flanxxl
+                st.session_state["llm_chain"] = flanxxl.build_chain()
+            elif sys.argv[1] == "openai":
+                st.session_state["llm_app"] = openai
+                st.session_state["llm_chain"] = openai.build_chain()
+            elif sys.argv[1] == "llama2chat":
+                st.session_state["llm_app"] = llama2chat
+                st.session_state["llm_chain"] = llama2chat.build_chain()
+            elif sys.argv[1] == "bedrockclaude":
+                st.session_state["llm_app"] = bedrockclaude
+                st.session_state["llm_chain"] = bedrockclaude.build_chain()
+            elif sys.argv[1] == "bedrocktitan":
+                st.session_state["llm_app"] = bedrocktitan
+                st.session_state["llm_chain"] = bedrocktitan.build_chain()
+            elif sys.argv[1] == "bedrockai21labs":
+                st.session_state["llm_app"] = bedrockai21labs
+                st.session_state["llm_chain"] = bedrockai21labs.build_chain()
+            else:
+                raise Exception("Unsupported LLM: ", sys.argv[1])
+        else:
+            raise Exception(
+                "Usage: streamlit run app.py <flanxl|flanxxl|llama2chat|bedrockclaude|bedrocktitan|bedrockai21labs|openai>"
+            )
+
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = []
+
+    if "chats" not in st.session_state:
+        st.session_state.chats = [{"id": 0, "question": "", "answer": ""}]
+
+    if "questions" not in st.session_state:
+        st.session_state.questions = []
+
+    if "answers" not in st.session_state:
+        st.session_state.answers = []
+
+    if "input" not in st.session_state:
+        st.session_state.input = ""
+
+    st.markdown(
+        """
+            <style>
+                   .block-container {
+                        padding-top: 32px;
+                        padding-bottom: 32px;
+                        padding-left: 0;
+                        padding-right: 0;
+                    }
+                    .element-container img {
+                        background-color: #000000;
+                    }
+                    .main-header {
+                        font-size: 24px;
+                    }
+                    #MainMenu {
+                        visibility: visible;
+                    }
+                    footer {
+                        visibility: visible;
+                    }
+                    header {
+                        visibility: visible;
+                    }
+            </style>
+            """,
+        unsafe_allow_html=True,
+    )
+
+    clear = write_top_bar()
+
+    if clear:
+        st.session_state.questions = []
+        st.session_state.answers = []
+        st.session_state.input = ""
+        st.session_state["chat_history"] = []
+
+    with st.container():
+        for q, a in zip(st.session_state.questions, st.session_state.answers):
+            write_user_message(q)
+            write_chat_message(a, q)
+
+    st.markdown("---")
+
+    if SHOW_SAMPLE_QUESTIONS:
+        with st.expander("Click here for sample questions..."):
+            st.markdown(
+                """
+                    - What is Amazon SageMaker?
+                    - What are some of its major features?
+                    - How do I get started using it?
+                    - Tell me about Amazon SageMaker Feature Store.
+                    - What does the Inference Recommender do?
+                    - What is Autopilot?
+                    - How much does SageMaker cost?
+                """
+            )
+        st.markdown(" ")
+
+    st.text_input(
+        TEXT_INPUT_PROMPT,
+        placeholder=TEXT_INPUT_PLACEHOLDER,
+        help=TEXT_INPUT_HELP,
+        key="input",
+        on_change=handle_input,
+    )
+
+
+def write_top_bar():
+    col1, col2, col3 = st.columns([1, 10, 2])
+    with col1:
+        st.image(HEADER_LOGO, use_column_width="always")
+    with col2:
+        selected_provider = sys.argv[1]
+        if selected_provider in PROVIDER_MAP:
+            provider = PROVIDER_MAP[selected_provider]
+        else:
+            provider = selected_provider.capitalize()
+        # st.markdown(f"#### {HEADER_TITLE}!")
+        st.markdown(f"#### {HEADER_TITLE} and {provider}!")
+    with col3:
+        clear = st.button("Clear Chat")
+    return clear
+
+
+def handle_input():
+    input = st.session_state.input
+    question_with_id = {"question": input, "id": len(st.session_state.questions)}
+    st.session_state.questions.append(question_with_id)
+
+    chat_history = st.session_state["chat_history"]
+    if len(chat_history) == MAX_HISTORY_LENGTH:
+        chat_history = chat_history[:-1]
+
+    llm_chain = st.session_state["llm_chain"]
+    chain = st.session_state["llm_app"]
+    result = chain.run_chain(llm_chain, input, chat_history)
+    answer = result["answer"]
+    chat_history.append((input, answer))
+
+    document_list = []
+    if "source_documents" in result:
+        for d in result["source_documents"]:
+            if not (d.metadata["source"] in document_list):
+                document_list.append((d.metadata["source"]))
+
+    st.session_state.answers.append(
+        {
+            "answer": result,
+            "sources": document_list,
+            "id": len(st.session_state.questions),
+        }
+    )
+    st.session_state.input = ""
+
+
+def write_user_message(md):
+    col1, col2 = st.columns([1, 12])
+
+    with col1:
+        st.image(USER_ICON, use_column_width="always")
+    with col2:
+        st.warning(md["question"])
+
+
+def render_answer(answer):
+    col1, col2 = st.columns([1, 12])
+    with col1:
+        st.image(AI_ICON, use_column_width="always")
+    with col2:
+        st.info(answer["answer"])
+
+
+def render_sources(sources):
+    col1, col2 = st.columns([1, 12])
+    with col2:
+        with st.expander("Sources"):
+            for s in sources:
+                st.write(s)
+
+
+def write_chat_message(md, q):
+    chat = st.container()
+    with chat:
+        render_answer(md["answer"])
+        if SHOW_DOC_SOURCES:
+            render_sources(md["sources"])
+
+
+if __name__ == "__main__":
+    main()
---
+++ model_providers/kendra_chat_open_ai.py
@@ -0,0 +1,58 @@
+import os
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.retrievers import AmazonKendraRetriever
+
+# ***** CONFIGURABLE PARAMETERS *****
+MODEL_NAME = os.environ.get("MODEL_NAME", "gpt-3.5-turbo")
+TEMPERATURE = os.environ.get("TEMPERATURE", 1e-10)
+MAX_TOKENS = os.environ.get("MAX_TOKENS", 512)
+KENDRA_INDEX_ID = os.environ["KENDRA_INDEX_ID"]
+MAX_HISTORY_LENGTH = 5
+# ******************************************************************
+
+
+def build_chain():
+    llm = ChatOpenAI(temperature=TEMPERATURE, max_tokens=MAX_TOKENS, model=MODEL_NAME)
+
+    retriever = AmazonKendraRetriever(index_id=KENDRA_INDEX_ID)
+
+    prompt_template = """
+  The following is a friendly conversation between a human and an AI. 
+  The AI is talkative and provides lots of specific details from its context.
+  If the AI does not know the answer to a question, it truthfully says it 
+  does not know.
+  {context}
+  Instruction: Based on the above documents, provide a detailed answer for, {question} Answer "don't know" 
+  if not present in the document. 
+  Solution:"""
+    prompt = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    condense_qa_template = """
+  Given the following conversation and a follow up question, rephrase the follow up question 
+  to be a standalone question.
+
+  Chat History:
+  {chat_history}
+  Follow Up Input: {question}
+  Standalone question:"""
+    standalone_question_prompt = PromptTemplate.from_template(condense_qa_template)
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever,
+        condense_question_prompt=standalone_question_prompt,
+        return_source_documents=True,
+        combine_docs_chain_kwargs={"prompt": prompt},
+    )
+    return qa
+
+
+def run_chain(chain, prompt: str, history=None):
+    if history is None:
+        history = []
+    return chain({"question": prompt, "chat_history": history})
